6def1c9d33d32a301aa56978e1f87a88ea354a83
IAKarasev <iakarsev@gmail.com>
2025-11-23 00:12:12 +0300

    removed hl groups, optimized code

--- lua/myterm.lua
+++ lua/myterm.lua
@@ -6,28 +6,43 @@ M.MyTerm = {
 	win_id = -1,
 	win_h = 12,
 	float_win_id = -1,
-	float_win_name = " MyTerm ",
-	active_mode = "",
 }
 
-local default_config = {
-	usrCmd = {
+M.default_config = {
+	split_win = {
+		height = 12,
+	},
+	float_win = {
+		float_win_name = " MyTerm ",
+		float_width = 0.8,
+		float_height = 0.8,
+	},
+	usr_cmd = {
+		enabled = true,
 		toggleSplit = "MyTermSplit",
 		toggleFloat = "MyTermFloat",
 		sendCmd = "MyTermCmd",
+		info = "MyTermInfo",
 	},
 	keys = {
+		enabled = true,
 		toggleSplit = "<leader>tt",
 		toggleFloat = "<leader>tf",
 		sendCmd = "<leader>te",
 	},
+	hl = {
+		split_win = {},
+		float_win = {},
+	},
 }
 
+M.config = {}
+
 M.DefaultConfig = function()
-	return default_config
+	return M.default_config
 end
 
-M.Show = function()
+M.ShowInfo = function()
 	vim.print(vim.inspect(M.MyTerm))
 end
 
@@ -38,25 +53,14 @@ M.CreateTerminalBuff = function()
 		M.MyTerm.job_id = vim.bo.channel
 	end)
 
-	-- vim.api.nvim_buf_set_keymap(buf, "n", "<super>bb", function()
-	-- 	vim.notify("ESC is pressed")
-	-- 	local win_id = vim.api.nvim_get_current_win()
-	-- 	if win_id == M.MyTerm.float_win_id then
-	-- 		vim.api.nvim_win_hide(M.MyTerm.float_win_id)
-	-- 	elseif win_id == M.MyTerm.win_id then
-	-- 		M.MyTerm.win_h = vim.api.nvim_win_get_height(M.MyTerm.win_id)
-	-- 		vim.api.nvim_win_close(M.MyTerm.win_id, true)
-	-- 	end
-	-- end, { noremap = false })
-
 	vim.keymap.set("n", "<Esc>", function()
-		vim.print("Hide terminal")
 		local cur_win = vim.api.nvim_get_current_win()
-		if win_id == M.win_id then
-			M.MyTerm.win_h = vim.api.nvim_win_get_height(M.MyTerm.win_id)
-			vim.api.nvim_win_close(cur_win, true)
-		elseif win_id == M.float_win_id then
-			vim.api.nvim_win_close(cur_win, true)
+		if cur_win == M.MyTerm.win_id then
+			vim.cmd("set ls=2")
+			M.MyTerm.win_h = vim.api.nvim_win_get_height(cur_win)
+			vim.api.nvim_win_hide(cur_win)
+		elseif cur_win == M.MyTerm.float_win_id then
+			vim.api.nvim_win_hide(cur_win)
 		end
 	end, { buffer = buf })
 
@@ -64,70 +68,48 @@ M.CreateTerminalBuff = function()
 	return buf
 end
 
-M.SetBufferKeyHide = function(buffer)
-	print("buffer num: " .. buffer)
-	vim.keymap.set("n", "<super>bb", function()
-		vim.notify("ESC is pressed")
-		local win_id = vim.api.nvim_get_current_win()
-		if win_id == M.MyTerm.float_win_id then
-			vim.api.nvim_win_hide(M.MyTerm.float_win_id)
-		elseif win_id == M.MyTerm.win_id then
-			M.MyTerm.win_h = vim.api.nvim_win_get_height(M.MyTerm.win_id)
-			vim.api.nvim_win_close(M.MyTerm.win_id, true)
-		end
-	end, { buffer = buffer, noremap = false })
-end
-
-M.NewTermVsplit = function()
+M.NewTermHsplit = function()
 	vim.cmd.vnew()
 	vim.cmd.wincmd("J")
+	vim.cmd("set ls=0")
 	vim.api.nvim_win_set_height(0, M.MyTerm.win_h)
 	M.MyTerm.win_id = vim.api.nvim_get_current_win()
 
-	if vim.api.nvim_buf_is_valid(M.MyTerm.buff_id) then
-		local tmpbuf = vim.api.nvim_win_get_buf(M.MyTerm.win_id)
-		vim.cmd.buffer(M.MyTerm.buff_id)
-		vim.api.nvim_buf_delete(tmpbuf, { force = true })
-	else
-		-- vim.cmd.term()
-		-- M.MyTerm.job_id = vim.bo.channel
-		-- M.MyTerm.buff_id = vim.fn.bufnr()
-		-- M.SetBufferKeyHide(M.MyTerm.buff_id)
-		local buf = M.CreateTerminalBuff()
-		vim.cmd.buffer(buf)
+	local buf = M.MyTerm.buff_id
+	if not vim.api.nvim_buf_is_valid(buf) then
+		buf = M.CreateTerminalBuff()
 	end
-	-- vim.api.nvim_command("startinsert")
+	vim.cmd.buffer(buf)
+	vim.api.nvim_command("startinsert")
 end
 
 M.ToggleMyTermHsplit = function()
 	if not vim.api.nvim_win_is_valid(M.MyTerm.win_id) then
-		M.NewTermVsplit()
+		M.NewTermHsplit()
 	else
+		vim.cmd("set ls=2")
 		M.MyTerm.win_h = vim.api.nvim_win_get_height(M.MyTerm.win_id)
-		vim.api.nvim_win_close(M.MyTerm.win_id, true)
+		vim.api.nvim_win_hide(M.MyTerm.win_id)
 	end
 end
 
 local function CreateMyTermFloating(opts)
 	-- Get current UI dimensions
-	local width = opts.width or vim.api.nvim_get_option("columns")
-	local height = opts.height or vim.api.nvim_get_option("lines")
+	local width = opts.width or vim.api.nvim_get_option_value("columns", {})
+	local height = opts.height or vim.api.nvim_get_option_value("lines", {})
 
-	-- Calculate 80% of screen dimensions
-	local win_width = math.floor(width * 0.8)
-	local win_height = math.floor(height * 0.8)
+	-- Calculate float win size
+	local win_width = math.floor(width * M.config.float_win.float_width)
+	local win_height = math.floor(height * M.config.float_win.float_height)
 
 	-- Calculate centered position
 	local row = math.floor((height - win_height) / 2)
 	local col = math.floor((width - win_width) / 2)
 
 	-- Get or Create term buff
-	local buf = nil
-	if vim.api.nvim_buf_is_valid(M.MyTerm.buff_id) then
-		buf = M.MyTerm.buff_id
-	else
-		buf = vim.api.nvim_create_buf(false, true)
-		M.SetBufferKeyHide(buf)
+	local buf = M.MyTerm.buff_id
+	if not vim.api.nvim_buf_is_valid(M.MyTerm.buff_id) then
+		buf = M.CreateTerminalBuff()
 	end
 
 	-- Set window configuration
@@ -139,7 +121,7 @@ local function CreateMyTermFloating(opts)
 		row = row,
 		col = col,
 		border = "rounded",
-		title = { { M.MyTerm.float_win_name, "MyTermFloatBorder" } },
+		title = { { M.config.float_win.float_win_name } },
 		title_pos = "center",
 	}
 
@@ -156,11 +138,7 @@ M.ToggleFloatTerm = function(opts)
 			vim.cmd.term()
 			M.MyTerm.job_id = vim.bo.channel
 		end
-		vim.api.nvim_win_set_option(
-			M.MyTerm.float_win_id,
-			"winhl",
-			"Normal:MyTermFloatBg,FloatBorder:MyTermFloatBorder"
-		)
+		vim.api.nvim_command("startinsert")
 	else
 		vim.api.nvim_win_hide(M.MyTerm.float_win_id)
 	end
@@ -168,41 +146,45 @@ end
 
 M.SendCmdToTerm = function()
 	local cmd = vim.fn.input("to MyTerm")
-	vim.notify(M.MyTerm.buff_id)
 	if vim.api.nvim_buf_is_valid(M.MyTerm.buff_id) and M.MyTerm.job_id > 0 then
-		vim.fn.chansend(M.MyTerm.job_id, { cmd .. "\r\n" })
+		vim.fn.chansend(M.MyTerm.job_id, { cmd })
 	end
 end
 
 M.setup = function(opts)
-	opts = vim.tbl_deep_extend("force", default_config, opts)
-	vim.api.nvim_set_hl(0, "MyTermFloatBg", { bg = "#111317" })
-	vim.api.nvim_set_hl(0, "MyTermFloatBorder", { bg = "#111317", fg = "#589ED7" })
+	-- load config
+	M.config = vim.tbl_deep_extend("force", M.default_config, opts)
+	M.MyTerm.win_h = M.config.split_win.height
 
-	vim.keymap.set("n", opts.keys.toggleSplit, M.ToggleMyTermHsplit, { desc = "Toggle MyTerm in hsplit" })
+	-- setting highlight groups
+	local hl_split_win = vim.api.hl
+	if M.config.hl.split_win then
+	end
 
-	vim.keymap.set("n", opts.keys.sendCmd, M.SendCmdToTerm, { desc = "Send bash cmd to MyTerm" })
+	-- Key mappings
+	if M.config.keys.enabled then
+		vim.keymap.set("n", M.config.keys.toggleSplit, M.ToggleMyTermHsplit, { desc = "Toggle MyTerm in hsplit" })
 
-	vim.keymap.set("n", opts.keys.toggleFloat, function()
-		M.ToggleFloatTerm({})
-	end, { desc = "Toggle MyTerm in floating window" })
+		vim.keymap.set("n", M.config.keys.sendCmd, M.SendCmdToTerm, { desc = "Send bash cmd to MyTerm" })
 
-	if opts.usrCmd.toggleSplit and opts.usrCmd.toggleSplit ~= "" then
+		vim.keymap.set("n", M.config.keys.toggleFloat, function()
+			M.ToggleFloatTerm({})
+		end, { desc = "Toggle MyTerm in floating window" })
+	end
+
+	-- User Commands
+	if M.config.usr_cmd.enabled then
 		vim.api.nvim_create_user_command(
-			opts.usrCmd.toggleSplit,
+			M.config.usr_cmd.toggleSplit,
 			M.ToggleMyTermHsplit,
 			{ desc = "Toggle my term hsplit" }
 		)
-	end
-	if opts.usrCmd.toggleFloat and opts.usrCmd.toggleFloat ~= "" then
-		vim.api.nvim_create_user_command(opts.usrCmd.toggleFloat, function()
+		vim.api.nvim_create_user_command(M.config.usr_cmd.toggleFloat, function()
 			M.ToggleFloatTerm({})
 		end, { desc = "Toogle MyTerm floating window" })
+		vim.api.nvim_create_user_command(M.config.usr_cmd.sendCmd, M.SendCmdToTerm, { desc = "Send command to MyTerm" })
+		vim.api.nvim_create_user_command(M.config.usr_cmd.info, M.ShowInfo, { desc = "Show myterm specs" })
 	end
-	if opts.usrCmd.sendCmd and opts.usrCmd.sendCmd ~= "" then
-		vim.api.nvim_create_user_command(opts.usrCmd.sendCmd, M.SendCmdToTerm, { desc = "Send command to MyTerm" })
-	end
-	vim.api.nvim_create_user_command("MyTermShow", M.Show, { desc = "Show myterm specs" })
 end
 
 return M
